7a77043ae7218f8ae3d72a5243592faa2b326f10
rjszz <1225475362@qq.com>
2020-04-18 22:44:51 +0800

    Site updated: 2020-04-18 22:44:51

--- dist/music.js
+++ dist/music.js
@@ -5,9 +5,9 @@ const ap = new APlayer({
     audio: [
 	
 	  {
-        name: "巴赫：G大调第一组曲 前奏曲",
-        artist: '让德隆',
-        url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_9826525&response=res&type=convert_url&',
+        name: "巴赫(G大调第一大提琴组曲)前奏曲",
+        artist: '秦立巍',
+        url: 'https://sharefs.yun.kugou.com/202004182242/6061ba8be4dc0dee6270d7814d9cd84f/G206/M07/08/07/Dg4DAF5pkK2AQ-j2ACsCj_EuT2s463.mp3',
         cover: 'https://pic.xiami.net/images/pic/04/02/10051096837m.jpg?x-oss-process=image/resize,limit_0,m_fill,s_410/quality,q_80/format,jpg',
       },
     ]
